3a2a2a34dd75be59386d7d4b2f44ac33320bd7e6
Andrey Ulanov <andy.ulanov@gmail.com>
2024-05-06 15:28:19 +0400

    Initial Commit

---
+++ Auth/AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthApp.swift
+//  Auth
+//
+//  Created by Andrey Ulanov on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
